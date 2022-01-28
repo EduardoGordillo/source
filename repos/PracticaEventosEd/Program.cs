470d80635969b68a8e0ad2425521223d0712a74e
EduardoGordillo <94868224+EduardoGordillo@users.noreply.github.com>
2022-01-28 14:08:45 -0600

    get add

---
+++ repos/PracticaEventosEd/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+
+namespace PracticaEventosEd
+{ 
+    public enum TipoAlerta
+{
+    Error = 1,
+    Advertencia = 2,
+    Exito = 3
+}
+
+public enum TipoPago
+{
+    Tarjeta = 1,
+    Transferencia = 2,
+    Otros = 3
+}
+   // public delegate void CambioFormaPagoHandler(TipoPago tipoPago, TipoAlerta alerta);
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingresa una forma de pago");
+            Console.WriteLine("1 - Tarjeta \n2 -Tranferencia \n3- Otros");
+            string tipoPago = Console.ReadLine();
+
+            FormaDePago fm = new FormaDePago();
+            fm.CambioFormaPago += fm_selecionFormaPago;
+
+            fm.CambioFormaPago += fm_continuarProcesoPago;
+            fm.Tipo = (TipoPago)Enum.Parse(typeof(TipoPago), tipoPago);
+            
+        }
+
+        static void fm_selecionFormaPago(object emisor, CambioFormaPagoEventArgs args )
+        {
+            if (args.TipoAlerta.Equals(TipoAlerta.Error))
+
+                Console.WriteLine("error, elegiste una forma de pago incorrecta");
+            else if (args.TipoAlerta.Equals(TipoAlerta.Exito))
+                Console.WriteLine("Forma de pago sleccionanda: {0}", args.TipoPago.ToString());
+
+
+
+        } 
+        static void fm_continuarProcesoPago( object emisor, CambioFormaPagoEventArgs args )
+        {
+            bool estatus = false;
+            if (args.Equals(TipoAlerta.Exito))
+            {
+
+                Console.WriteLine("continuando con el proceso de pago por ", args.ToString());
+                Console.WriteLine("Presione X para continuar..", args.ToString());
+                string tipoPago = Console.ReadLine();
+                if (tipoPago == "x")
+                    estatus = true;
+            }
+            Console.WriteLine("confirmacion recibida, estatus de la operacion {0}", estatus ? "confirmada" : "cancelada");
+        }
+    }
+public class FormaDePago // event broadcaste /emisora
+{
+    private TipoPago tipo;
+        public event EventHandler<CambioFormaPagoEventArgs> CambioFormaPago;
+        public TipoPago Tipo
+        {
+            get { return tipo; }
+            set
+            {
+                TipoAlerta TipoAlerta = TipoAlerta.Error;
+                if (value.Equals(TipoPago.Tarjeta)
+                    || value.Equals(TipoPago.Transferencia)
+                        || value.Equals(TipoPago.Otros))
+                {
+                    tipo = value;
+                    TipoAlerta = TipoAlerta.Exito;
+                }
+                CambioFormaPagoEventArgs args = new CambioFormaPagoEventArgs
+                {
+                    TipoPago = tipo,
+                    TipoAlerta = TipoAlerta
+                };
+                CambioFormaPago(this, args);
+            }
+           
+        }
+      
+
+
+
+
+    
+   }
+    public class CambioFormaPagoEventArgs : EventArgs
+    {
+        public TipoAlerta TipoAlerta { get; set; }
+        public TipoPago TipoPago { get; set; }
+    }
+}
--- repos/Vuelos/Program.cs
+++ repos/Vuelos/Program.cs
@@ -7,6 +7,8 @@ using System.Threading.Tasks;
 namespace Vuelos
 {
     public delegate float CalcularTotal(float subtotal);
+    public delegate void CalcularTotalRef(ref float subtotal);
+    public delegate void ImprimirMensaje(string msj);
     class VueloNacional
     {
         float Iva
@@ -65,7 +67,13 @@ namespace Vuelos
                 return total + ImpuestoFederalSeguridad;
             return total;
         }
-
+        public void CalcularTotalConImpuestos( ref float monto)
+        {
+            float total = monto + (monto * Iva) + Tua;
+            if (Destino == 559)
+                total += ImpuestoFederalSeguridad;
+            monto = total;
+        }
     }
 
     class Program
@@ -89,9 +97,51 @@ namespace Vuelos
             /*CalcularTotal total1 = vueloInter.CalcularMontoTotal;
             float precio1 = 8800.00F;
             Console.WriteLine("Costo de vuelo internacional {0}", total1(precio1));*/
+            float precio1 = 9800f;
             total = vueloInter.CalcularMontoTotal;
-            float t = total(precio);
+            float t = total(precio1);
             Console.WriteLine("precio de vuelo internacional {0}", t);
+
+            #region Parametros
+            float totalAdultoMayor = CalcularConDescuentoAdultoMayor(t, total);
+            Console.WriteLine("Costo vuelo internacional con descuento adulto mayor {0}", totalAdultoMayor);
+            #endregion
+
+            #region multicast
+            CalcularTotal totalB = vueloInter.CalcularMontoTotal;
+            totalB += CalcularTotalSeguro;
+            Console.WriteLine("Calcular del seguro {0}", totalB(precio1));
+            CalcularTotalRef tr = vueloInter.CalcularTotalConImpuestos;
+            tr += CalcularTotalConSeguroRef;
+            tr(ref precio1);
+            Console.WriteLine("Costo de vuelo internacional con seguro {0}", precio1);
+            #endregion
+            #region anonimos
+            ImprimirMensaje im = delegate (string mensaje)
+            {
+                Console.WriteLine("mesnaje {0}", mensaje);
+
+            };
+            im("Delegado comes");
+            #endregion
+        }
+        static float CalcularConDescuentoAdultoMayor(float monto, CalcularTotal total)
+        {
+            float subtotal = total(monto);
+            return subtotal - (0.35f * subtotal);
+                
+        }
+        static float CalcularTotalSeguro(float total)
+        {
+            float porcentajeSeguro = 0.1f;
+            return total * porcentajeSeguro;
+
+        }
+        static void CalcularTotalConSeguroRef(ref float total)
+        {
+            float porcentajeSeguro = 0.1F;
+            total += total * porcentajeSeguro;
+        }
         }
     }
-}
+
