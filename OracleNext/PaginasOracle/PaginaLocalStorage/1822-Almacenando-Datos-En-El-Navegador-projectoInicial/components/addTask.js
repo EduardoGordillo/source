e5813bf9edcdfa4e53f51c409db74c81a471934e
Eduardo Gordillo <live_carr@hotmail.com>
2022-11-07 12:58:48 -0600

    local storage parte III

--- OracleNext/PaginasOracle/PaginaLocalStorage/1822-Almacenando-Datos-En-El-Navegador-projectoInicial/components/addTask.js
+++ OracleNext/PaginasOracle/PaginaLocalStorage/1822-Almacenando-Datos-En-El-Navegador-projectoInicial/components/addTask.js
@@ -2,29 +2,38 @@ import checkComplete from './checkComplete.js';
 import deleteIcon from './deleteIcon.js';
 
 const addTask = (evento)=>{ 
-    const list = document.querySelector('[data-list]');
     evento.preventDefault();
+    const list = document.querySelector('[data-list]');
     const taskList = JSON.parse(localStorage.getItem('Task') ) || [];
-    console.log(taskList);
     const input = document.querySelector('[data-form-input]');
     const calendar = document.querySelector('[data-form-date]');
+
     const value = input.value;
     const date = calendar.value;
     const dateFormat = moment(date).format('DD/MM/YYYY')
-    input.value = '';  
-  
-    taskList.push({value, dateFormat});
-    localStorage.setItem('Task', JSON.stringify(taskList));
-    
-   
-    const task = createTask({value, dateFormat});
-    list.appendChild(task);
 
+    input.value = '';  
+    calendar.value = '';
+    if(value === '' || date === ''){
+    
+      return 'Invalid'
+    }
+    else{
+      taskList.push({value, dateFormat});
+      localStorage.setItem('Task', JSON.stringify(taskList));
+      
+    
+      const task = createTask({value, dateFormat});
+      list.appendChild(task);
+    
+    }
+
+    
   }
   
   
   
-const createTask = ({value, dateFormat}) => {
+export const createTask = ({value, dateFormat}) => {
     
    
     const task = document.createElement('li');
@@ -34,17 +43,17 @@ const createTask = ({value, dateFormat}) => {
     const taskContent = document.createElement('div');
   
     const titleTask = document.createElement('span');
-    titleTask.classList.add('task');
-    titleTask.innerText = value;
-    taskContent.appendChild(checkComplete());
-    taskContent.appendChild(titleTask);
+        titleTask.classList.add('task');
+        titleTask.innerText = value;
+        taskContent.appendChild(checkComplete());
+        taskContent.appendChild(titleTask);
     // task.innerHTML = content;
     const dateElement = document.createElement('span');
-    dateElement.innerHTML= dateFormat;
-    
-    task.appendChild(taskContent);
-    task.appendChild(dateElement);
-    task.appendChild(deleteIcon());
+        dateElement.innerHTML= dateFormat;
+        
+        task.appendChild(taskContent);
+        task.appendChild(dateElement);
+        task.appendChild(deleteIcon());
     return task;
   };
   
--- OracleNext/PaginasOracle/PaginaLocalStorage/1822-Almacenando-Datos-En-El-Navegador-projectoInicial/components/deleteIcon.js
+++ OracleNext/PaginasOracle/PaginaLocalStorage/1822-Almacenando-Datos-En-El-Navegador-projectoInicial/components/deleteIcon.js
@@ -2,12 +2,14 @@ const deleteIcon = () => {
   const i = document.createElement('i');
   i.classList.add('fas', 'fa-trash-alt', 'trashIcon', 'icon');
   i.addEventListener('click', deleteTask);
+ 
   return i;
 };
 
 const deleteTask = (event) => {
   const parent = event.target.parentElement;
-  parent.remove();
+   parent.remove()
+  
 };
 
 export default deleteIcon;
